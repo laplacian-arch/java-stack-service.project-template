f97833cd374a64b00a4798c2cc5a4b50331112d2
iwauo <iwauo.tajima@gmail.com>
2021-01-28 01:51:35 +0900

    update deployment script

--- src/template/scripts/{each environments as environment}/deploy-on-{hyphen environment.name}@main@.hbs.sh
+++ src/template/scripts/{each environments as environment}/deploy-on-{hyphen environment.name}@main@.hbs.sh
@@ -1,19 +1,28 @@
 {{define 'deployment_components' (unique (map environment.deployments '@it.component')) ~}}
+{{define 'deployments_with_migration' (unique (filter environment.deployments '@it.initial_data')) ~}}
 DEPLOYMENT_BASE_DIR=$PROJECT_BASE_DIR/dest/environments/{{hyphen environment.name}}
 COMPONENT_BASE_DIR=$PROJECT_BASE_DIR/dest/components
 
 main () {
+  build_components
+  build_migration_tasks
+  deploy_containers
+}
+
+build_components() {
+  echo "Building deployable components..."
   {{#each deployment_components as |component| ~}}
   {{#if component.custom_built ~}}
   build_{{lower-snake component.name}}
   {{/if}}
   {{/each}}
-  migrate_data
-  deploy_containers
 }
 
-migrate_data() {
-  echo "Migrating application data and schema..."
+build_migration_tasks() {
+  echo "Building data migration tasks..."
+  {{#each deployments_with_migration as |deployment| ~}}
+  build_{{lower-snake deployment.name}}_migration
+  {{/each}}
 }
 
 deploy_containers() {
